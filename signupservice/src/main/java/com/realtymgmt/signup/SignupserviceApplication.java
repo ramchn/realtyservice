2f891f4b8e70d1e795a95b48931e41604fa64a95
ramchn <ramamoorthyp@gmail.com>
2020-06-22 14:56:39 +0530

    signup service created

---
+++ signupservice/src/main/java/com/realtymgmt/signup/SignupserviceApplication.java
@@ -0,0 +1,13 @@
+package com.realtymgmt.signup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignupserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignupserviceApplication.class, args);
+	}
+
+}
