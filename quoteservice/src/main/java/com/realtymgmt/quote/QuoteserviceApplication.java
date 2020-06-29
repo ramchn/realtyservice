1c2206659026992ebdd43062d6bf88b30bf8619f
ramchn <ramamoorthyp@gmail.com>
2020-06-29 09:53:30 +0530

    comment updated

--- quoteservice/src/main/java/com/realtymgmt/quote/QuoteserviceApplication.java
+++ quoteservice/src/main/java/com/realtymgmt/quote/QuoteserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuoteserviceApplication {
 
 	public static void main(String[] args) {
-		// test comment
+		// test comment 123
 		SpringApplication.run(QuoteserviceApplication.class, args);
 	}
 
