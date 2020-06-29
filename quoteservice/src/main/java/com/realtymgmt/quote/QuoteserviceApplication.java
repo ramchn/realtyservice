9ada3c3a8435851049eec396215f1074c67e87c9
ramchn <ramamoorthyp@gmail.com>
2020-06-29 10:11:52 +0530

    added one java file

--- quoteservice/src/main/java/com/realtymgmt/quote/QuoteserviceApplication.java
+++ quoteservice/src/main/java/com/realtymgmt/quote/QuoteserviceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuoteserviceApplication {
 
 	public static void main(String[] args) {
-		// test comment 123
+		// test comment 123456
 		SpringApplication.run(QuoteserviceApplication.class, args);
 	}
 
