912d6e25a4832bfafab17f15d950a2db42ee5eff
ramchn <ramamoorthyp@gmail.com>
2020-06-29 09:48:18 +0530

    new service created

---
+++ quoteservice/src/main/java/com/realtymgmt/quote/QuoteserviceApplication.java
@@ -0,0 +1,13 @@
+package com.realtymgmt.quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteserviceApplication.class, args);
+	}
+
+}
