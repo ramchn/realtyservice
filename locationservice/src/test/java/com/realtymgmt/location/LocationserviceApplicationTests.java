68ffebb14a958a1a97d12d0b35e1f7b56fabb13f
ramchn <ramamoorthyp@gmail.com>
2020-07-04 14:11:27 +0530

    location service added (get cities, states, countries)

---
+++ locationservice/src/test/java/com/realtymgmt/location/LocationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtymgmt.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
