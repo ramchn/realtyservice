9b1b683be3547389aa0e463698324e836e3996d8
ramchn <ramamoorthyp@gmail.com>
2020-07-16 20:02:20 +0530

    service updated

--- oauth2service/src/main/java/com/realtymgmt/oauth2/Oauth2serviceApplication.java
+++ oauth2service/src/main/java/com/realtymgmt/oauth2/Oauth2serviceApplication.java
@@ -1,12 +1,19 @@
 package com.realtymgmt.oauth2;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@RestController
 public class Oauth2serviceApplication {
 
 	public static void main(String[] args) {
@@ -21,5 +28,10 @@ public class Oauth2serviceApplication {
 				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
+	}
+	
+	@GetMapping("/user")
+	Principal getUser(Principal user) {
+		return user;
 	}
 }
--- oauth2service/src/main/java/com/realtymgmt/oauth2/config/ServerConfig.java
+++ oauth2service/src/main/java/com/realtymgmt/oauth2/config/ServerConfig.java
@@ -4,22 +4,15 @@ package com.realtymgmt.oauth2.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ServerConfig extends GlobalAuthenticationConfigurerAdapter {
 
 	@Autowired
 	DataSource datasource;
-	
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
 	
 	public void init(AuthenticationManagerBuilder auth) throws Exception {
 		auth
